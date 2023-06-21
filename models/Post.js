e1fd47f69029c3373237f58d163965715d2fd32e
HA JIN <w2128@e-mirim.hs.kr>
2023-06-21 20:58:46 +0900

    /community/.. -> 게시글 ID로 글 내용 가져오기

--- models/Post.js
+++ models/Post.js
@@ -37,11 +37,11 @@ Post.getPost = (postID) => {
                 console.log(err);
                 reject(err);
             } else {
+                resolve(row);
+                console.log('게시글 하나 가져오기!')
                 conn.query('UPDATE posts SET post_views = post_views + 1 WHERE post_id = ?', postID, (err, row) => {
                     if(err) reject(err)
-                    // else resolve(row);
                 })
-                resolve(row)
             }
             
         })
@@ -61,11 +61,20 @@ Post.getReplyAll = (postID) => {
 }
 Post.addReply = (reply) => {
     return new Promise((resolve, reject) => {
-        let sql = 'INSERT INTO reply (post_id, nickname, reply_content, reply_date) VALUES(?,?,?,?)';
+        let sql = 'INSERT INTO reply (post_id, nickname, reply_content, reply_date, reply_profile) VALUES(?,?,?,?,?)';
         //[reply.post_id, reply.nickname, reply.reply_content, reply.reply_date]
-        conn.query(sql, [reply.post_id, reply.nickname, reply.reply_content, reply.reply_date], (err, res) => {
-            if(err) reject(err);
-            else resolve(res);
+        conn.query(sql, [reply.post_id, reply.nickname, reply.reply_content, reply.reply_date,reply.reply_profile], (err, res) => {
+            if(err){
+                reject(err);
+            } else {
+                let sql = 'UPDATE posts SET comments = comments +1 WHERE post_id = ?';
+                conn.query(sql, [reply.post_id], (updateErr, updateRes) => {
+                    if(err) {
+                        reject(updateErr);
+                    }
+                    resolve(updateRes);
+                })
+            } 
         })
     })
 }
@@ -73,8 +82,12 @@ Post.getPostByCategory = (category) => {
     return new Promise((resolve, reject) => {
         let sql = 'SELECT * FROM posts WHERE category = ? ORDER BY upload_date DESC';
         conn.query(sql, category, (err, rows) => {
-            resolve(rows);
-        })
+            if(err){
+                reject(err);
+            } else {
+                resolve(rows)
+            }
+        });
     })
 }
 
