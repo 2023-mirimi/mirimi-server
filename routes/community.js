7303ec2f84bca0cda7a0e8530c8e741dc092ef83
HA JIN <w2128@e-mirim.hs.kr>
2023-06-21 20:58:02 +0900

    /community/... -> 파라미터로 카테고리를 입력 받아 특정 데이터 가져오기

--- routes/community.js
+++ routes/community.js
@@ -8,5 +8,5 @@ router.get('/reply/:id', community.getReply) //게시글 댓글 가져오기
 router.post('/', community.setPost) //게시글 db 추가하기
 router.post('/:id', community.addReply)
 //카테고리별로 데이터 가져오기
-// router.get('/:category', community.getPostByCategory)
+router.get('/tab/:category', community.getPostByCategory)
 module.exports = router;
